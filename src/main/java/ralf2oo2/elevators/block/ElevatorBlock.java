8a3645ebd74ac28a828de472965e907e677d262c
Ralf2oo2 <ralf2oo2@hotmail.com>
2025-04-21 22:55:54 +0200

    Added item models

--- src/main/java/ralf2oo2/elevators/block/ElevatorBlock.java
+++ src/main/java/ralf2oo2/elevators/block/ElevatorBlock.java
@@ -73,111 +73,18 @@ public class ElevatorBlock extends TemplateBlock {
     }
 
     private boolean useDye(World world, int x, int y, int z, PlayerEntity player){
+        if(player.getHand().getDamage() >= Color.values().length) return false;
+
         BlockState elevatorBlockState = world.getBlockState(x, y, z);
         Direction direction = elevatorBlockState.get(ElevatorBlock.DIRECTION_ENUM_PROPERTY);
+        Color dyeColor = Color.values()[player.getHand().getDamage()];
         int blockMeta = world.getBlockMeta(x, y, z);
-        boolean usedDye = false;
-        switch (player.getHand().getDamage()){
-            case 0:
-                if(color != Color.BLACK && colors.containsKey(Color.BLACK)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.BLACK).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 1:
-                if(color != Color.RED && colors.containsKey(Color.RED)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.RED).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 2:
-                if(color != Color.GREEN && colors.containsKey(Color.GREEN)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.GREEN).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 3:
-                if(color != Color.BROWN && colors.containsKey(Color.BROWN)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.BROWN).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 4:
-                if(color != Color.BLUE && colors.containsKey(Color.BLUE)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.BLUE).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 5:
-                if(color != Color.PURPLE && colors.containsKey(Color.PURPLE)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.PURPLE).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 6:
-                if(color != Color.CYAN && colors.containsKey(Color.CYAN)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.CYAN).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 7:
-                if(color != Color.LIGHT_GRAY && colors.containsKey(Color.LIGHT_GRAY)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.LIGHT_GRAY).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 8:
-                if(color != Color.GRAY && colors.containsKey(Color.GRAY)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.GRAY).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 9:
-                if(color != Color.PINK && colors.containsKey(Color.PINK)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.PINK).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 10:
-                if(color != Color.LIME && colors.containsKey(Color.LIME)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.LIME).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 11:
-                if(color != Color.YELLOW && colors.containsKey(Color.YELLOW)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.YELLOW).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 12:
-                if(color != Color.LIGHT_BLUE && colors.containsKey(Color.LIGHT_BLUE)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.LIGHT_BLUE).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 13:
-                if(color != Color.MAGENTA && colors.containsKey(Color.MAGENTA)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.MAGENTA).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 14:
-                if(color != Color.ORANGE && colors.containsKey(Color.ORANGE)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.ORANGE).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-            case 15:
-                if(color != Color.WHITE && colors.containsKey(Color.WHITE)){
-                    world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(Color.WHITE).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
-                    usedDye = true;
-                }
-                break;
-        }
-        if(usedDye){
+
+        if(color != dyeColor && colors.containsKey(dyeColor)){
+            world.setBlockStateWithMetadataWithNotify(x, y, z, colors.get(dyeColor).getDefaultState().with(ElevatorBlock.DIRECTION_ENUM_PROPERTY, direction), blockMeta);
             player.getHand().count--;
+            return true;
         }
-        return usedDye;
+        return false;
     }
 }
